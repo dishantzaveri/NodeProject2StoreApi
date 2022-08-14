f58c5b4e9fcb8c1d996f98a9ee54c2793336c51c
dishantzaveri <zaveridishant@gmail.com>
2022-08-14 14:24:36 +0530

    database connected

--- StoreAPI/starter/app.js
+++ StoreAPI/starter/app.js
@@ -9,8 +9,28 @@ const errorMiddleware = require('./middleware/error-handler');
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome!</h1>');
-}).listen(3000, () => {
-    console.log('Server is running on port 3000');
+    res.send('<h1>Welcome!</h1><a href ="/api/v1/products">products route</a>');
+})
+
+// app.use('/api/v1/products', require('./routes/products'));
+
+const connectDB =
+    require('./db/connect');
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log('connected to mongoose');
+        app.listen(port, console.log(`Server is running on port ${port}`));
+
+    } catch (e) {
+        console.log(e);
+    }
 }
-);
+
+start();
